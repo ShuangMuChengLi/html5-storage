e797b18e3164fcf5b09f6d802603d7f4b9db7181
lchaoqun <lchaoqun@sina.cn>
2017-12-15 15:37:17 +0800

    init

---
+++ demo/helloworld.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="vux-demo">
+            <h1></h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    let storage = require("../js/util/storageUtil.js");
+    export default {
+        components: {
+        },
+        data() {
+            return {
+
+            }
+        },
+        mounted(){
+            this.init();
+        },
+        methods: {
+            init() {
+                storage.setLocalStorage("name","1");
+                console.log(storage.getLocalStorage("name"));
+            }
+        }
+    }
+</script>
+
+<style>
+    .vux-demo {
+        text-align: center;
+    }
+
+    .logo {
+        width: 100px;
+        height: 100px
+    }
+</style>
